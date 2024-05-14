5edd39b83ce9bd8ba053370072249a4b625f842d
Hideki2512 <hideki.rafael@outlook.com.br>
2024-05-14 16:10:18 -0300

    Adição dados ao banco de dados

--- GestorDeEstudantesHidekiT6/Login_Form.cs
+++ GestorDeEstudantesHidekiT6/Login_Form.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace GestorDeEstudantesHidekiT6
 {
@@ -29,7 +30,27 @@ namespace GestorDeEstudantesHidekiT6
 
         private void buttonLogin_Click(object sender, EventArgs e)
         {
+            MeuBancoDeDados meuBancoDeDados = new MeuBancoDeDados();
 
+            MySqlDataAdapter meuAdaptadorSql = new MySqlDataAdapter();
+            DataTable minhaTabela = new DataTable();
+            MySqlCommand meuComandoSql = new MySqlCommand("SELECT * FROM `usuario` WHERE `nome_de_usuario` = @usuario AND `senha` = @senha", meuBancoDeDados.getConexao);
+
+            meuComandoSql.Parameters.Add("@usuario", MySqlDbType.VarChar).Value = textBoxUsuario.Text;
+            meuComandoSql.Parameters.Add("@senha", MySqlDbType.VarChar).Value = textBoxSenha.Text;
+
+            meuAdaptadorSql.SelectCommand = meuComandoSql;
+
+            meuAdaptadorSql.Fill(minhaTabela);
+
+            if (minhaTabela.Rows.Count > 0) 
+            {
+                MessageBox.Show("Existem dados!");
+            }
+            else
+            {
+                MessageBox.Show("Não foram encontrados dados.");
+            }
         }
     }
 }
--- GestorDeEstudantesHidekiT6/MeuBancoDeDados.cs
+++ GestorDeEstudantesHidekiT6/MeuBancoDeDados.cs
@@ -11,8 +11,8 @@ namespace GestorDeEstudantesHidekiT6
 {
     internal class MeuBancoDeDados
     {
-        private MySqlConnection conexao =
-            new MySqlConnection("datasources=localhost;port=3306;username=root;password=;database=sga_estudantes_bd_t6");
+        private MySqlConnection conexao = 
+            new MySqlConnection("datasource=localhost;port=3306;username=root;password=;database=sga_estudantes_bd_t6");
         
         public MySqlConnection getConexao
         {
